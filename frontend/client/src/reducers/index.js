1f86c54b004ffb32ce452711b2206205ada2de0b
nweerakeshara@gmail.com <thnuwanga>
2023-04-30 05:52:00 +1000

    applicant reducer action added

--- frontend/client/src/reducers/index.js
+++ frontend/client/src/reducers/index.js
@@ -1,11 +1,12 @@
 import {combineReducers} from "redux";
 import empReducer from './empReducer';
 import errorReducer from './errorReducer';
+import applicantReducer from './empReducer';
 
 
 export default combineReducers({
 
-
+    applicant : applicantReducer,
     emp : empReducer,
     error : errorReducer
 });
