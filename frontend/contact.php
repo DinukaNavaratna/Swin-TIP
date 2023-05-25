889af09804339d6505b8dbaaea47a8e2817a00c4
yash160301 <128664973+yash160301@users.noreply.github.com>
2023-05-25 12:06:39 +1000

    Update contact.php

--- frontend/contact.php
+++ frontend/contact.php
@@ -51,7 +51,7 @@ require 'php/header.php';
                 </div>
             </div>
             <div class="col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <iframe class="position-relative rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameborder="0" style="min-height: 400px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe class="position-relative rounded w-100 h-100" src="https://www.google.com/maps/place/Swinburne+University+of+Technology/@-37.8221504,145.0363797,17z/data=!3m1!4b1!4m6!3m5!1s0x6ad642326bae5aaf:0x75e96bbd4988f769!8m2!3d-37.8221504!4d145.0389546!16zL20vMDFrcV9z?entry=ttu" frameborder="0" style="min-height: 400px; border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
             <div class="col-md-6">
                 <div class="wow fadeInUp" data-wow-delay="0.5s">
